af702ad1cf7d728237a031ac3701de7fbaed811f
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-17 08:01:40 -0400

    modified class Blog

--- app/models.py
+++ app/models.py
@@ -40,7 +40,7 @@ class User(UserMixin,db.Model):
     def __repr__(self):
         return f'User {self.username} {self.bio} {self.email}'
 
-class blog(db.Model):
+class Blog(db.Model):
     __tablename__ = 'blog'
 
     id = db.Column(db.Integer, primary_key = True)
