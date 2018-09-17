aadfecf4cb564dcabbff3aa69706ace8538ae8de
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-17 05:01:02 -0400

    added a profile

---
+++ app/main/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Please, tell us something about you.',validators = [Required()])
+    submit = SubmitField('Submit')
