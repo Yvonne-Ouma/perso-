a0a02ae0ae0a0950f1d4f62495e91f9f7e5830f2
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-09-17 07:23:46 -0400

    sending message

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from . import auth
 from ..models import User
 from .forms import LoginForm,RegistrationForm 
 from flask_login import login_user,logout_user,login_required
-# from ..email import mail_message
+from ..email import mail_message
 
 
 @auth.route('/login',methods=['GET','POST'])
@@ -37,7 +37,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        # mail_message("Welcome to Blog app","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Blog app","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
